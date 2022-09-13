84b32d07392ccb87577e54008d669d77749bb1bd
k.nishizoe <k.nishizoe@timeconcier.jp>
2022-09-13 11:05:19 +0900

    v1.0.0 esbuild対応

--- deploy.js
+++ deploy.js
@@ -6,7 +6,7 @@ const auth = require('./auth.json');
     console.error('`auth.jsonを作成し、kintoneの権限情報を設定してください。`');
     return;
   }
-  console.log('🟡deploying ===================');
+  console.log('⏫ deploying ===================');
   await PowerShell.$`node ./node_modules/@kintone/customize-uploader/bin/cli manifest.json --domain ${auth.domain} --username ${auth.username} --password ${auth.password}`;
-  console.log('👍deployed ===================');
+  console.log('✅ deployed  ===================');
 })();
